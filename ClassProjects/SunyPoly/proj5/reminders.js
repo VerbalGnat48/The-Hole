91053dbc5e2ab1168868e8d50e22ba2aa58a82f8
verbalgnat48 <cmazz12345@gmail.com>
2025-11-06 19:56:07 -0500

    Adding my Game Theory project

---
+++ ClassProjects/SunyPoly/proj5/reminders.js
@@ -0,0 +1,16 @@
+function addTask() {
+	let taskInput = document.getElementById("task").value;
+	let dueDate = document.getElementById("due-date").value;
+	let taskList = document.getElementById("task-list");
+
+	if(taskInput && dueDate) {
+		let li = document.createElement("li");
+		li.innerHTML = `${taskInput} <span>${new Date(dueDate).toLocaleString()}</span>`;
+		taskList.appendChild(li);
+
+		let timeDiff = new Date(dueDate) - new Date();
+		if(timeDiff > 0) {
+			setTimeout(() => alert(`Reminder: ${taskInput} is due!`), timeDiff);
+		}
+	}
+}
