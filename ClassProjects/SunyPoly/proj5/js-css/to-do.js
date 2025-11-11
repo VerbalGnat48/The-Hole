94e020809894044a228bf247d69dcaa901fdd7b3
verbalgnat48 <cmazz12345@gmail.com>
2025-11-11 15:17:04 -0500

    Combined to-do and reminders

--- ClassProjects/SunyPoly/proj5/js-css/to-do.js
+++ ClassProjects/SunyPoly/proj5/js-css/to-do.js
@@ -1,22 +1,40 @@
+/////////////////////////////////////////////////
+// Values and Variables
+//
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
 
+/////////////////////////////////////////////////
+// Methods
+//
+
+//Add Task
 function addTask() {
+	let dueDate = document.getElementById("due-date").value;
 	if(inputBox.value === '') {
 		alert("You must write something!");
 	}
+	else if(dueDate === '') {
+		alert("You must provide a time!");
+	}
 	else {
 		let li = document.createElement("li");
-		li.innerHTML = inputBox.value;
+		li.innerHTML = `${inputBox.value} is due at ${new Date(dueDate).toLocaleString()}`;
 		listContainer.appendChild(li);
 		let span = document.createElement("span");
 		span.innerHTML = "\u00d7";
 		li.appendChild(span);
+
+		let timeDiff = new Date(dueDate) - new Date();
+			if(timeDiff > 0) {
+			setTimeout( () => alert(`Reminder: ${inputBox.value} is due!`), timeDiff);
+		}
 	}
 	inputBox.value = "";
 	saveData();
 }
-/*Complete/Delete Tasks*/
+
+//Add/Remove Task
 listContainer.addEventListener("click", function(e) {
 	if(e.target.tagName === "LI") {
 		e.target.classList.toggle("checked");
@@ -28,11 +46,31 @@ listContainer.addEventListener("click", function(e) {
 	}
 }, false);
 
+//Save Task
 function saveData() {
 	localStorage.setItem("data", listContainer.innerHTML);
 }
 
+//Show Task
 function showTask() {
 	listContainer.innerHTML = localStorage.getItem("data");
 }
+
+
+/////////////////////////////////////////////////
+// "main"
+//
 showTask();
+
+
+
+
+
+
+
+
+
+
+
+
+//
