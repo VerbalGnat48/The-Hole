c307d61dd6f3d852ae8ad072cd33e5d102f182d4
verbalgnat48 <cmazz12345@gmail.com>
2026-04-19 23:36:33 -0400

    All I have left is click buttons and their info

--- ClassProjects/SunyPoly/Info_Design_Final/final.js
+++ ClassProjects/SunyPoly/Info_Design_Final/final.js
@@ -9,6 +9,14 @@ const follower_05 = document.getElementById('follower_05');
 const follower_06 = document.getElementById('follower_06');
 const follower_07 = document.getElementById('follower_07');
 
+const follower_11 = document.getElementById('follower_11');
+const follower_12 = document.getElementById('follower_12');
+const follower_13 = document.getElementById('follower_13');
+const follower_14 = document.getElementById('follower_14');
+const follower_15 = document.getElementById('follower_15');
+const follower_16 = document.getElementById('follower_16');
+const follower_17 = document.getElementById('follower_17');
+
 window.addEventListener('mousemove', (e) => {
 	follower_01.style.left = e.clientX + 'px';
 	follower_01.style.top = e.clientY + 'px';
@@ -24,4 +32,19 @@ window.addEventListener('mousemove', (e) => {
 	follower_06.style.top = e.clientY + 'px';
 	follower_07.style.left = e.clientX + 'px';
 	follower_07.style.top = e.clientY + 'px';
+
+	follower_11.style.left = e.clientX + 'px';
+	follower_11.style.top = e.clientY + 'px';
+	follower_12.style.left = e.clientX + 'px';
+	follower_12.style.top = e.clientY + 'px';
+	follower_13.style.left = e.clientX + 'px';
+	follower_13.style.top = e.clientY + 'px';
+	follower_14.style.left = e.clientX + 'px';
+	follower_14.style.top = e.clientY + 'px';
+	follower_15.style.left = e.clientX + 'px';
+	follower_15.style.top = e.clientY + 'px';
+	follower_16.style.left = e.clientX + 'px';
+	follower_16.style.top = e.clientY + 'px';
+	follower_17.style.left = e.clientX + 'px';
+	follower_17.style.top = e.clientY + 'px';
 });
