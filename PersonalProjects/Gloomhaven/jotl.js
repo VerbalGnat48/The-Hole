7a7a67e9dd24213d06697e8f6493ddd481dbe871
verbalgnat48 <cmazz12345@gmail.com>
2026-05-29 20:18:01 -0400

    Added Enemy Adding logic and enemy container. Have to do enemy attribute display next

--- PersonalProjects/Gloomhaven/jotl.js
+++ PersonalProjects/Gloomhaven/jotl.js
@@ -78,7 +78,7 @@ function selectCharacterLevel(selectorID) {
 	const cardBox = document.getElementById(cardBoxID);
 
 	//Change Card Level Value
-	const level = selectorID.slice(-2);
+	const level = selectorID.slice(-1);
 	cardBox.dataset.level = level;
 
 	//Clear Selected Level
@@ -135,8 +135,30 @@ function setCharacters() {
 ////////////////////////////////////////////////////////////////////////////
 // Page 02 Functions
 //
-const characters = ["Demolitionist","Hatchet","RedGuard","Voidwarden"];
+const jotl_characters = ["Demolitionist","Hatchet","RedGuard","Voidwarden"];
+const jotl_health = [
+	[8,9,11,12,14,15,17,18,20],
+	[8,9,11,12,14,15,17,18,20],
+	[10,12,14,16,18,20,22,24,26],
+	[6,7,8,9,10,11,12,13,14]
+];
 var alive = [];
+
+const jotl_enemies = [
+	//Name, Health/Elite Health, Attack/Elite Attack
+	[ "Black Imp",	[3,4,5,5,7,9,10,13,4,6,8,8,11,14,15,19], [1,1,1,2,2,2,3,3,2,2,2,3,3,3,4,4] ],
+	[ "Black Sludge", [4,5,7,8,9,10,12,16,8,9,11,11,13,15,16,18], [2,2,2,3,3,3,4,4,2,2,3,3,4,4,4,5] ],
+	[ "Blood Imp", [3,4,5,5,7,8,9,12,4,6,7,10,11,13,17,21], [1,1,1,2,2,2,3,3,2,2,2,2,3,3,3,4] ],
+	[ "Chaos Demon", [7,8,11,12,15,16,20,22,10,12,14,18,21,26,30,35], [2,3,3,4,4,5,5,6,3,4,5,5,6,6,7,8] ],
+	[ "Giant Viper", [2,3,4,4,6,7,8,10,3,5,7,8,11,13,14,18], [1,1,1,2,2,3,3,3,2,2,2,3,3,3,4,4] ],
+	[ "Living Corpse", [5,7,9,10,11,13,14,16,10,11,14,14,16,18,23,29], [3,3,3,4,4,4,4,5,3,4,4,5,5,6,6,6] ],
+	[ "Rat Monstrosity", [4,4,5,6,8,10,12,12,6,7,8,10,12,13,14,16], [1,2,2,3,3,3,3,4,2,2,3,3,3,4,4,5] ],
+	[ "Stone Golem", [10,10,11,11,12,13,16,16,10,11,13,14,16,18,20,21], [3,3,4,4,4,5,5,5,4,4,5,5,6,6,7,7] ],
+	[ "Vermling Raider", [4,5,9,12,12,15,17,19,8,10,14,16,19,23,27,31], [2,2,2,2,3,3,3,4,2,2,3,4,4,4,5,6] ],
+	[ "Vermling Scout", [2,3,3,5,6,8,10,13,4,5,5,7,8,11,13,17], [1,1,2,2,3,3,3,3,2,2,3,3,4,4,4,4] ],
+	[ "Zealot", [4,6,7,8,10,12,14,16,7,8,11,13,17,18,22,26], [2,2,3,3,3,3,4,5,3,3,3,4,4,5,6,7] ],
+//	[ "", [], [] ],
+];
 
 function getCharacters() {
 	const selected = [];
@@ -167,19 +189,46 @@ function getCharacters() {
 	}
 	makeTables();
 	makeCharacterStats(selected, level);
+	makeEnemyList();
 }
 
 function makeCharacterStats(selected, level) {
 	//Make Attributes for selected characters
 	for (i=0; i<selected.length; i++) {
 		if (selected[i] === "true") {
-			//Create Attributes
+			//Create Attributes Box
 			const box = document.createElement("box-long");
-			box.id = characters[i] + "_Attributes";
-			box.textContent = "L:" + level[i] + " H:" + "1" + "/" + "1" + " S:" + "0";
-			document.getElementById(characters[i] + "_Container").appendChild(box);
+			box.id = jotl_characters[i] + "_Attributes";
+			box.style.flexWrap = "nowrap";
+			document.getElementById(jotl_characters[i] + "_Container").appendChild(box);
+
+			//Level
+			var element = document.createElement("p");
+			element.style.display = "inline-block";
+			element.id = jotl_characters[i] + "_Level";
+			element.textContent = "L:" + level[i];
+			box.append(element, " ", ".", " ");
+
+			//Health
+			element = document.createElement("p");
+			element.style.display = "inline-block";
+			var p = Number(level[i]) - 1;
+			element.id = jotl_characters[i] + "_Health";
+			element.textContent = "H:" + jotl_health[i][p] + "/" + jotl_health[i][p];
+			box.append(element, " ", ".", " ");
+
+			//Shield
+			element = document.createElement("p");
+			element.style.display = "inline-block";
+			element.id = jotl_characters[i] + "_Shield";
+			element.textContent = "S:0";
+			box.appendChild(element);
 		}
 	}
+}
+
+function getAttribute() {
+	
 }
 
 function makeTables() {
@@ -271,7 +320,39 @@ document.addEventListener("keypress", (event) => {
 	}
 });
 
+function makeEnemyList() {
+	const father = document.getElementById("Enemy_Selector");
+	for (i=0; i<jotl_enemies.length; i++ ) {
+		const option = document.createElement("option");
+		option.value = i;
+		option.text = jotl_enemies[i][0];
+		father.appendChild(option);
+	}
+}
 
+function addEnemy(selectorID) {
+	//Get Values set
+	const box = document.getElementById("Enemy_Box");
+	var start = 0;
+	var end = 8;
+	var color = "3px solid white";
+	if (selectorID.includes("Elite") ) {
+		start += 8;
+		end += 8;
+		color = "3px solid gold"
+	}
+	console.log(color);
+
+	//Make the Enemy
+	const selection = document.getElementById("Enemy_Selector");
+	const enemy = document.createElement("img");
+
+	enemy.src = "./Images/Enemies/" + jotl_enemies[selection.value][0].replace(/\s+/g,"") + ".png";
+	enemy.style.height = "93px";
+	enemy.style.width = "auto";
+	enemy.style.border = color;
+	box.appendChild(enemy);
+}
 
 
 
